75474437fe24d12e744387ec2dca3613031a72f4
谷幸东 <Paddygu@feelwx.com>
2018-05-16 10:50:39 +0800

    add 相机授权逻辑

--- camera/camera/ViewController.m
+++ camera/camera/ViewController.m
@@ -16,8 +16,17 @@
 @property (weak, nonatomic) IBOutlet UIButton *recordButton;
 
 
+// 配置信息
+typedef NS_ENUM(NSInteger,CameraSetupResult){
+    CameraSetupResultSuccess,
+    CameraSetupResultCameraNotAuthorized,
+    CameraSetupResultSessionConfigurationFailed
+};
+@property (nonatomic) CameraSetupResult setupResult;
 
+// session
 @property (nonatomic) AVCaptureDeviceDiscoverySession *videoDeviceDiscoverySession;
+@property (nonatomic) dispatch_queue_t sessionQueue;    // 用于创建串行队列
 
 @end
 
@@ -30,7 +39,39 @@
     NSArray *deviceTypes = @[AVCaptureDeviceTypeBuiltInWideAngleCamera,AVCaptureDeviceTypeBuiltInTelephotoCamera,AVCaptureDeviceTypeBuiltInDuoCamera];
     self.videoDeviceDiscoverySession = [AVCaptureDeviceDiscoverySession discoverySessionWithDeviceTypes:deviceTypes mediaType:AVMediaTypeVideo position:AVCaptureDevicePositionBack];
     
+    // 创建串行队列用于session之间的通信
+    self.sessionQueue = dispatch_queue_create("session queue", DISPATCH_QUEUE_SERIAL);
     
+    // 初始化默认值
+    self.setupResult = CameraSetupResultSuccess;
+    // 检查相机的权限
+    AVAuthorizationStatus videoStatus = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+    switch (videoStatus) {
+        case AVAuthorizationStatusAuthorized:       //  已授权
+            NSLog(@"已授权");
+            break;
+        case AVAuthorizationStatusNotDetermined:    //  还没询问是否开启
+        {   // TODO:没搞明白此处为什么一定要加括号作为一个代码块
+            NSLog(@"没有询问是否开启");
+            // 此时需要将串行队列上的任务挂起
+            dispatch_suspend(self.sessionQueue);
+            // 请求权限
+            [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
+                if (!granted) {// 如果没有准许
+                    self.setupResult = CameraSetupResultCameraNotAuthorized;
+                }
+                // 队列上的任务恢复执行
+                dispatch_resume(self.sessionQueue);
+            }];
+            break;
+        }
+        case AVAuthorizationStatusDenied:           //  未授权
+            NSLog(@"未授权");
+            break;
+        
+        default:
+            break;
+    }
 
     
 }
